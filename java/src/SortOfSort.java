762766c586d5bb3acd7156b42871e41a093fe08f
menynexi <meny_nexi@outlook.com>
2020-03-02 21:16:00 -0700

    polished version

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,7 +1,7 @@
-public class SortOfSort {
+public class SortOfSort{
     public static int SortOfSortCount;
 
-    public static void sortOfSort(int[] arr) {
+    public void sortOfSort(int[] arr) {
         SortOfSortCount = 0;
         int counterA = 2;
         int counterB = 2;
@@ -14,22 +14,17 @@ public class SortOfSort {
             int temp;
 
             for (int i = firstIndex; i <= lastIndex; ++i) {
-                //System.out.println("I have cheacked this value" + arr[i] + " it is not more than " + max);
                 if (max < arr[i]){
                     maxIndex = i;
                     max = arr[i];
-                    //System.out.println("this is the index of the max value " + maxIndex);
-                    //System.out.println("The max value is " + max);
                 }
             }
 
             if (counterA != 0) {
-                //System.out.println("This came here once");
                 temp = arr[lastIndex];
                 arr[lastIndex] = max;
                 arr[maxIndex] = temp;
                 lastIndex = lastIndex - 1;
-                //make a flag methad that woks better I think flags will work
                 counterA--;
                 if (counterA == 0) {
                     counterB = 2;
@@ -40,10 +35,6 @@ public class SortOfSort {
                 temp = arr[firstIndex];
                 arr[firstIndex] = max;
                 arr[maxIndex] = temp;
-
-                //System.out.println("This is the temp " + temp);
-                //System.out.println("This is the arr[firstIndex] " + arr[firstIndex] + " And this is its index " + firstIndex);
-                //System.out.println("This is the value of arr[maxIndex]" + arr[maxIndex] + " and thi sis its index " + maxIndex);
                 firstIndex++;
                 counterB--;
                 if (counterB == 0) {
@@ -53,32 +44,13 @@ public class SortOfSort {
 
             SortOfSortCount++;
             print(arr);
-            //System.out.println(counterA);
-            //System.out.println(counterB);
-            //System.out.println(lastIndex);
-            //System.out.println(firstIndex);
         }
     }
 
-    public static void print(int[] arr) {
+    public void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println("");
     }
-
-
-    /* public static void main(String[] args){ //I used this as my first test case instead of creating  an new file
-        int[] arr = {7, 8, 6, 9, 3};
-        sortOfSort(arr);
-    } */
 }
-   /* public static int greatest(int[] arr){
-        int max = arr[0];
-        for(int i = 0; i < arr.length; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }*/
--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -1,1 +1,14 @@
+//import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SortOfSortTester{
+
+    @Test
+    public void test1(){
+        int[] arr = {7, 8, 6, 9, 3};
+        int[] expectedArr = {7,6,3,8,9}};
+        //assertEquals(expectedArr, sortOfSort(arr));
+    }
+
 
